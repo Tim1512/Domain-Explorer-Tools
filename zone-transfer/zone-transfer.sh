0b1d47a79f78f7ea3b021b26df89262982d1004e
Giovani Ferreira <silva.giovanif@gmail.com>
2016-08-14 17:20:15 -0300

    Add script structure: args and help

--- zone-transfer/zone-transfer.sh
+++ zone-transfer/zone-transfer.sh
@@ -11,31 +11,108 @@
 # Developer - Giovani Ferreira
 #------------------------------------------------------------------------------------------------------------
 
-# Check if the domain was given
-if [ -z $1 ]; then
-    echo -e "Incorret usage, should consider pass the domain:\n$0 website.com"
-    # Abort the script if no URL was given
-    exit 1
-fi
+OUTPUT="zone_file.txt"                  # Output file of the program
+URL=""                                  # Target url for the attack
+QUIET=false                             # Operation mode quiet
+VERBOSE=false                           # Operation mode verbose
 
-# Output file of the program
-RESULT="zone_file.txt"
+function main {
+    echo "--> Starting zone transfer attempt in $URL"
 
-echo "--> Starting zone transfer attempt in $1"
+    echo $URL
 
-# Discovering all DNS servers of the domain
-for server in $(host -t ns $1 | cut -d " " -f4 ); do
-    # Get only the useful information in the zone file
-    host -l $1 $server | grep -E 'has address|name server' | sed 's/\.$//' >> $RESULT # Maybe "| tee -a $RESULT" instead of ">> $RESULT" for real time feedback
-    # Check if zone transfer was successful 
-    if [ ${PIPESTATUS[0]} -eq 0 ]; then
-        echo -e "--> Successful zone transfer in server: $server\n--> Results are in $RESULT"
-        # Exit program with success
-        exit 0
+    # Discovering all DNS servers of the domain
+    for server in $(host -t ns $URL | cut -d " " -f4 ); do
+        # Get only the useful information in the zone file
+        host -l $URL $server | grep -E 'has address|name server' | sed 's/\.$//' >> $OUTPUT # Maybe "| tee -a $OUTPUT" instead of ">> $OUTPUT" for real time feedback
+        # Check if zone transfer was successful 
+        if [ ${PIPESTATUS[0]} -eq 0 ]; then
+            echo -e "--> Successful zone transfer in server: $server\n--> Results are in $OUTPUT"
+            # Exit program with success
+            exit 0
+        fi
+        # If zone transfer was not successful
+        echo "--> Failed zone transfer in $server"
+    done
+
+    # If the program still running, there are no results
+    echo "--> Unable to do zone transfer in $URL"
+
+    return 1;
+}
+
+function parse_args {
+    if [ $# -eq 0 ]; then           # Check if at least one arg was passed
+        display_help
+        exit 1
     fi
-    # If zone transfer was not successful
-    echo "--> Failed zone transfer in $server"
-done
 
-# If the program still running, there are no results
-echo "--> Unable to do zone transfer in $1"
+    while (( "$#" )); do            # Stays in the loop as long as the number of parameters is greater than 0
+        case $1 in                  # Switch through cases to see what arg was passed
+            -V|--version) 
+                echo ":: Author: Giovani Ferreira"
+                echo ":: Source: https://github.com/giovanifss/Domain-Explorer-Tools"
+                echo ":: License: GPLv3"
+                echo ":: Version: 0.2"
+                exit 0;;
+
+            -o|--output-file)       # Get the next arg to be the output file
+                if [ -z $2 ] || [[ $2 == -* ]]; then
+                    error_with_message "Expected argument after output file option"
+                fi
+                OUTPUT=$2
+                shift;;             # To ensure that the next parameter will not be evaluated again
+
+            -q|--quiet)             # Set the program operation mode to quiet
+                if $VERBOSE; then   # Program can not behave quiet and verbose at same time
+                    error_with_message "Operation mode can not be quiet and verbose at the same time"
+                fi
+                QUIET=true;;
+
+            -v|--verbose)           # Set the program opeartion mode to verbose 
+                if $QUIET; then     # Program can not behave quiet and verbose at same time
+                    error_with_message "Operation mode can not be quiet and verbose at the same time"
+                fi
+                VERBOSE=true;;
+
+            -h|--help)              # Display the help message
+                display_help
+                exit 0;;
+
+            *)                      # If a different parameter was passed
+                if [ ! -z "$URL" ] || [[ $1 == -* ]]; then
+                    error_with_message "Unknow argument $1"
+                fi
+
+                URL=$1;;
+        esac
+        shift                       # Removes the element used in this iteration from parameters
+    done
+
+    if [ -z $URL ]; then
+        error_with_message "The target url must be passed"
+    fi
+
+    return 0
+}
+
+function display_help {
+    echo
+    echo ":: Usage: zone-trasfer [URL] [-o OUTPUT FILE] "
+    echo
+    echo ":: URL: The target url to gather information. MUST be a reachable domain."
+    echo ":: OUTPUT: The file to store the output generated. Use '-o | --output-file'"
+    echo ":: VERBOSE|QUIET: Operation mode can be specified by '-v|--verbose' or '-q|--quiet'"
+    echo ":: VERSION: To see the version and useful informations, use '-V|--version'"
+
+    return 0
+}
+
+function error_with_message {
+    echo ":: Error: $1"
+    echo ":: Use -h for help"
+    exit 1
+}
+
+parse_args $@
+main
